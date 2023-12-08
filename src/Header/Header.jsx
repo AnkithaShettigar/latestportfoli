b6f5342337fe49f2f0d2cef593f632bbe43d5ca1
AnkithaShettigar <ankitha.yermal@gmail.com>
2023-12-08 15:14:05 +0530

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="head">
+      <div className="head-left">
+        <span className="name">Ankitha</span>
+      </div>
+      <div className="head-right">
+        <div>{/* <FontAwesomeIcon icon="fa-solid fa-brightness" /> */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
